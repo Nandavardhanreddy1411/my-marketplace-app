cf1959427d6baa2c94ea23199e980a55ef01a3b6
Nandavardhan Reddy <nandavardhanreddy2002@gmail.com>
2026-05-16 20:42:20 +0530

    Fix: Wrap App with AppProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async'
+import { AppProvider } from './context/CartContext.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
+    <AppProvider>
       <App />
-    </HelmetProvider>
+    </AppProvider>
   </React.StrictMode>,
 )
